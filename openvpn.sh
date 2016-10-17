3c459f4b41476581aac3f36ddd71e40bbccc2d6e
MuluuJelekk <muluujelekk@gmail.com>
2016-10-18 00:54:14 +0800

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -4,10 +4,10 @@
 cd
 
 # install openvpn
-wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/arieonline/autoscript/master/conf/openvpn-debian.tar"
+wget -O /etc/openvpn/openvpn.tar "https://raw.githubusercontent.com/MuluuJelekk/openvpn/master/openvpn-debian.tar"
 cd /etc/openvpn/
 tar xf openvpn.tar
-wget -O /etc/openvpn/1194.conf "https://raw.github.com/arieonline/autoscript/master/conf/1194.conf"
+wget -O /etc/openvpn/1194.conf "https://raw.github.com/MuluuJelekk/AutoFix/master/80.conf"
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
@@ -21,13 +21,13 @@ service openvpn restart
 
 # configure openvpn client config
 cd /etc/openvpn/
-wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/arieonline/autoscript/master/conf/1194-client.conf"
-sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
+wget -O /etc/openvpn/80-client.ovpn "https://raw.github.com/arieonline/autoscript/master/conf/1194-client.conf"
+sed -i $MYIP2 /etc/openvpn/80-client.ovpn;
 PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
 useradd -M -s /bin/false KangArie
 echo "KangArie:$PASS" | chpasswd
 echo "KangArie" > pass.txt
 echo "$PASS" >> pass.txt
-tar cf client.tar 1194-client.ovpn pass.txt
+tar cf client.tar 80-client.ovpn pass.txt
 cp client.tar /home/vps/public_html/
 cd
